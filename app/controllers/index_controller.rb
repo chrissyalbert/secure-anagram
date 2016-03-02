0f5378feeba208a3fc26d26cfe978d5ab2c1620c
Chrissy Albert <chrissy_albert@yahoo.com>
2016-03-02 17:16:02 -0500

    index_controller fails lesson 18 anagrams_spec

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,8 +4,35 @@ end
 
 post '/' do
     #handle form submission
-    @word = params[:word]
-    redirect "/anagrams/#{@word}"
+    def three_letters?(input)
+        if input.length == 3
+            true
+        else
+            false
+        end
+    end
+    def distinct_letters?(input)
+        letter_array = input.chars
+        unique_letters = letter_array.uniq
+        if unique_letters.length < letter_array.length
+            false
+        else
+            true
+        end
+    end
+    def valid_input?(input)
+        if input.length > 3
+            raise Exception.new("Word must be less than or equal to 3 characters.")
+        end
+    end
+        word = params[:word]
+    begin
+        valid_input(word)
+        redirect "/anagrams/#{word}"
+    rescue Exception => error
+        @error = "Please enter a three letter word with no repeating letters."
+        erb :index
+    end
 end
 
 get '/anagrams/:word' do
